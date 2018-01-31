5256f990fe6be6858aa240c83e7ca770a51c88eb
안영동(Youngdong An) <youngdongan@gmail.com>
2018-01-31 17:40:55 +0900

    Initial commit

---
+++ src/main/java/com/flycamel/vertxjcache/VertxJcacheApplication.java
@@ -0,0 +1,12 @@
+package com.flycamel.vertxjcache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VertxJcacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VertxJcacheApplication.class, args);
+	}
+}
